873e8483f269c1d2584c6dee4aae7396fc2bb235
FranciscoLi2000 <liyufeng577@gmail.com>
2025-10-12 20:06:13 +0000

    C

--- C_01/ft_ultimate_div_mod.c
+++
@@ -1,9 +0,0 @@
-
-void	ft_ultimate_div_mod(int *a, int *b)
-{
-	int	tmp;
-
-	tmp = *a / *b;
-	*b = *a % *b;
-	*a = tmp;
-}
--- C_09/libft_creator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cc -Wall -Wextra -Werror -c ft_putchar.c
-cc -Wall -Wextra -Werror -c ft_swap.c
-cc -Wall -Wextra -Werror -c ft_putstr.c
-cc -Wall -Wextra -Werror -c ft_strlen.c
-cc -Wall -Wextra -Werror -c ft_strcmp.c
-ar rc libft.a *.o
-ranlib libft.a
